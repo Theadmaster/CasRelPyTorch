21cc3a8b6a48ecf3172b625d14c57636a3fa4259
theadmaster <55393861+Theadmaster@users.noreply.github.com>
2024-05-08 19:08:42 +0800

    修改数据

--- data/duie/process.py
+++ data/duie/process.py
@@ -8,5 +8,25 @@ def generate_rel():
             id += 1
     with open('rel.json', 'w',  encoding='utf-8') as f:
         json.dump(dict, f, ensure_ascii=False)
+
+def process_data():
+    res = []
+    with open('dev.json', 'r', encoding='utf-8') as f:
+        for line in f:
+            data = json.loads(line.strip())
+            text = data['text']
+            spo_list = data['spo_list']
+            new_spo_list = []
+            for spo in spo_list:
+                item = {'predicate': spo['predicate'], 'subject_type': spo['subject_type'], 'object_type': spo['object_type']['@value'], 'subject': spo['subject'], 'object': spo['object']['@value']}
+                new_spo_list.append(item)
+            res.append({'text': text, 'spo_list': new_spo_list})
+    with open('dev.json', 'w', encoding='utf-8') as f:
+        for line in res:
+            json.dump(line, f, ensure_ascii=False)
+            f.write('\n')
+
+
 if __name__ == '__main__':
-    generate_rel()
+    # generate_rel()
+    process_data()
